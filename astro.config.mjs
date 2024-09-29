839a39fa6409e1c37cb12d757bd8ef45f246d1e1
Matthew LeFevre <matthewlefevre95@gmail.com>
2024-09-29 17:05:22 -0600

    Initial Commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import netlify from '@astrojs/netlify';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), tailwind()],
+  output: 'server',
+  adapter: netlify()
+});
